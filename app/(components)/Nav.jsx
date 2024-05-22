eef71d0f13228398c1b062525c320f1986819295
toyocodes <toyjad@gmail.com>
2024-05-22 18:34:15 +0100

    fixed minor issues in production build

--- app/(components)/Nav.jsx
+++ app/(components)/Nav.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const Nav = () => {
   return (
-    <nav className="flex justify-between bg-nav p-4">
+    <nav className=" bg-nav p-4">
       <div className="flex items-center space-x-4">
         <Link href="/">
           <FaHome className="text-white" />
@@ -13,9 +13,7 @@ const Nav = () => {
           <FaTicket className="text-white" />
         </Link>
       </div>
-      <div>
-        <p className=" text-default-text">jake.lower17@gmail.com</p>
-      </div>
+     
     </nav>
   );
 };
